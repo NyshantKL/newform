c1bdd7f93b4f3d7617cca8e6363ba2247d3349b5
vijaypan <vscp1409@gmail.com>
2018-05-19 21:59:30 +0530

    aa

--- FinalSolution/Default.aspx.cs
+++ FinalSolution/Default.aspx.cs
@@ -17,7 +17,7 @@ namespace FinalSolution
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            ConfigurationManager.ConnectionStrings[""]
+            //ConfigurationManager.ConnectionStrings[""]
         }
     }
 }
