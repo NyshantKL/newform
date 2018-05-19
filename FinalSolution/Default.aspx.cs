0d9eaf20fe1337923aa72e5a2ff370e1b5142a8b
vijaypan <vscp1409@gmail.com>
2018-05-20 00:02:11 +0530

    db connection success

--- FinalSolution/Default.aspx.cs
+++ FinalSolution/Default.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,7 +19,37 @@ namespace FinalSolution
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //ConfigurationManager.ConnectionStrings[""]
+            DataTable dt = new DataTable();
+            int rows = 0;
+            try
+            {
+               if(txtFName.Text.Trim()!=string.Empty && txtLName.Text.Trim() != string.Empty)
+                
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ftbl"].ConnectionString))
+                {
+                    using (SqlCommand comm = new SqlCommand())
+                    {
+                        comm.Connection = con;
+                        con.Open();
+                        comm.CommandText = "INSERT INTO formtable values(@fname,@lname);";
+                        comm.Parameters.AddWithValue("@fname", txtFName.Text);
+                        comm.Parameters.AddWithValue("@lname", txtLName.Text);
+                          rows=comm.ExecuteNonQuery();
+                        //dt.Load(comm.ExecuteReader());
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                //Response.Write("Rows:" + dt.Rows.Count);
+                Response.Write("Rows:" + rows);
+            }
+                
         }
     }
 }
